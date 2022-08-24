9b1d813dcd0221c5491942f4d359f412b68b46b2
Cynthia Pardede <62656001+Html19@users.noreply.github.com>
2022-08-24 17:49:51 +0700

    add perubahan

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Cynthia v");
-        System.out.println("Hello, Pardede");
+        System.out.println("Hello, Veronika Pardede");
         System.out.println("Hello, Veronika");
     }
 }
